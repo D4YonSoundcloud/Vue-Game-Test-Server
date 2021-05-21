5721d3f9e4a043b45f1417cb6ee522f9afed64ff
d4yonsouncloude <mattd4y@gmail.com>
2021-05-21 16:56:54 -0400

    will it work?

--- Server.js
+++ Server.js
@@ -21,11 +21,7 @@ console.log('bruh')
 
 const server = http.createServer(app);
 
-const io = new Server(server, {
-	cors: {
-		origin: '*',
-	}
-});
+const io = new Server(server);
 
 console.log('io is set to the right server')
 
